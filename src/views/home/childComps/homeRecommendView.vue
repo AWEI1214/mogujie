9c333cd13b13d588825efece54a43d78285754c8
阿威 <2279085675@qq.com>
2021-07-13 21:59:47 +0800

    main

---
+++ src/views/home/childComps/homeRecommendView.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-07-13 21:50:38
+ * @LastEditTime: 2021-07-13 21:51:11
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vue\supermall\src\views\home\childComps\homeRecommendView.vue
+-->
+<template></template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
