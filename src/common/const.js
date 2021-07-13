4e7bf4cc5b688094429b74d6647b7c366c409cb2
阿威 <2279085675@qq.com>
2021-07-13 09:19:47 +0800

    main

---
+++ src/common/const.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-13 09:17:53
+ * @LastEditTime: 2021-07-13 09:17:54
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue\supermall\src\common\const.js
+ */
